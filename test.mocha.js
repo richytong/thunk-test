37282ed19f45591e605e044f7fd5905dc07bb6ad
Richard <richytong@gmail.com>
2026-07-29 17:13:17 -0700

    test

--- test.mocha.js
+++ test.mocha.js
@@ -224,9 +224,6 @@ describe('thunk-test', () => {
       assert.equal(Test.repr(Buffer.from([1, 2, 3])), 'Buffer([1, 2, 3])')
       function identity(value) { return value }
       assert.equal(Test.repr(identity), 'identity')
-      assert.equal(Test.repr(() => {
-}), `() => {
-}`)
       assert.equal(Test.repr(() => { const a = 1; return a }), '() => { const a = 1; return a }')
       assert.equal(Test.repr(new Set([1, 2, 3])), 'Set([1, 2, 3])')
       assert.equal(Test.repr(new Set([{ a: 1 }, { b: 2 }, { c: 3 }])), 'Set([{ a: 1 }, { b: 2 }, { c: 3 }])')
